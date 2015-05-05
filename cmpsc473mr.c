93a348991437682deb5d22dfa2b2dacc1a3599b1
Chinmay Garg <cag5525@psu.edu>
2015-05-05 02:35:00 -0400

    Finalized and got it working with thread

--- cmpsc473mr.c
+++ cmpsc473mr.c
@@ -4,26 +4,18 @@
 int count = 0;
 int temp_buffer_location = 0;
 int number_split = 0;
-int memoryLocation[10];
+int memoryLocation[10];	//no longer used
 int N=0, B=0;
+double timer_start =0;
+double timer_end = 0;	
+
+pthread_t *threads;
+queue** sharedBuffer;
+int* sharedBufferStatusForAdder;
 
 
 word_list *head = NULL;
 
-
-//just a basic function to return the minimum of two numbers
-
-/*
-int min(int x, int y)
-{
-	if(x > y)
-	{
-		return y;
-	}
-
-	return x;
-}
-*/
 
 void close_file(FILE* file)
 {
@@ -31,8 +23,117 @@ void close_file(FILE* file)
 }
 
 
+void init()
+{
+	int k = 0;
+	while(k < N)
+	{
+		sharedBuffer[k] = create_list();
+		sharedBufferStatusForAdder[k] = 0;
+		k++;
+	}
+
+}
+
 void *map_reader_i(void *ptr)
 {	
+	/*wordBuffer *word;
+	word = (wordBuffer*)ptr;
+	word_list *sharedMapBuffer = NULL;
+	sharedMapBuffer = malloc(sizeof(word_list));
+	
+	pthread_mutex_t lock;
+	pthread_t adder_i;
+	pthread_mutex_init(&lock, NULL);
+	char *c;
+	//printf("1\n");
+	//printf("%s\n", word->string);
+	//printf("2\n");
+	do
+    {
+    	c = sscanf(word->string, "%[A-Z|a-z]", sharedMapBuffer->word);
+    	
+    	printf("%s is a word\n", sharedMapBuffer->word);
+    	sscanf(word->string, "%*[^A-Za-z]");
+    	sharedMapBuffer= sharedMapBuffer->next;
+
+    	sharedMapBuffer = malloc(sizeof(word_list));
+    }while(c != EOF);
+
+    sharedMapBuffer = malloc(sizeof(word_list));
+	head = sharedMapBuffer;
+
+	adderBuffer *node = -malloc(sizeof(adderBuffer));
+	node->lock = lock;
+	node->list = sharedMapBuffer;
+	node->index = index;
+
+	node->count = 0;
+
+
+	pthread_create(&adder_i, NULL, map_adder_i, (void*)ptr);
+
+    c = strtok(word->string, " ,.*^!@~'");
+    while(c != NULL)
+    {
+    	//printf("%s\n", c);
+    	while(sharedMapBuffer->size == B);
+
+    	pthread_mutex_lock(&lock);
+    	sharedMapBuffer->word = malloc(sizeof(char*)*strlen(c)+1);
+    	sharedMapBuffer->word = c;
+    	c = strtok(NULL, " ");
+    	sharedMapBuffer = sharedMapBuffer->next;
+    	sharedMapBuffer = malloc(sizeof(word_list));
+    	pthread_mutex_unlock(&lock);
+    }
+
+
+    printf("Printing from the head\n");
+    while(head != NULL)
+    {
+    	printf("%s\n", head->word);
+    	head = head->next;
+    }
+    node->read_before = 1;
+
+    pthread_exit(NULL);*/
+    pthread_t adderthread;
+
+	readerBuffer *rdata = (readerBuffer *)ptr; // rdata 
+	int index = rdata->index;
+	char *chunk = rdata->chunk;
+	queue *wordlist = create_list();
+
+
+	pthread_mutex_t lock;
+
+	pthread_mutex_init(&lock, NULL);
+
+
+	adderBuffer *word_data = malloc(sizeof(adderBuffer));
+	word_data->adderlist = wordlist;
+	word_data->index = index;
+	word_data->read_before = 0;
+	word_data->lock = lock;
+
+	pthread_create(&adderthread, NULL, map_adder_i, (void*)word_data);
+	char *ptrs = strtok(chunk, " \n\r\t;:.!?',\"-\0");
+	
+	
+	while (ptrs != NULL){
+		while(wordlist->size == B);
+		pthread_mutex_lock(&lock);
+		enqueue (wordlist, 1, ptrs);
+		//printf("Enqueuing\n");
+		pthread_mutex_unlock(&lock);
+		ptrs = strtok(NULL,  " \n\r\t;:.!?',\"-\0");
+	}
+
+	word_data->read_before = 1;
+
+
+	pthread_exit(NULL);
 
 	
 }
@@ -40,46 +141,131 @@ void *map_reader_i(void *ptr)
 
 void *map_adder_i(void *ptr)
 {	
-	
 
+	adderBuffer *word_data = (adderBuffer*)ptr;
+
+	int index = word_data->index;
+	queue* wordlist = word_data->adderlist;
+	pthread_mutex_t lock = word_data->lock;
+
+	while(!word_data->read_before || wordlist->size > 0)
+	{ 
+		//printf("1\n");
+		while ( wordlist->size == 0 ); 
+		//printf("2\n");
+
+		pthread_mutex_lock(&lock);	
+		node* wordsList = dequeue(wordlist);
+	 	pthread_mutex_unlock(&lock);
+	 	
+	 	//sharedBuffer = malloc(sizeof(queue**)*10);
+		//sharedBuffer[index] = create_list();
+		node* word_find = look(sharedBuffer[index], wordsList->word);
+
+	 	if (word_find == NULL) 
+	 	{
+	 		enqueue(sharedBuffer[index], wordsList->count, wordsList->word);
+	 		//printf("Enqueuing now....lets see if this works..\n");
+	 	}
+	 	else 
+	 	{
+	 		word_find->count = word_find->count + wordsList->count;
+	 	}
+	 	free(wordsList);
+	 	//printf("I am here\n");
+	}
+
+	sharedBufferStatusForAdder[index] = 1;
+
+
+	pthread_exit(NULL);
 }
 
-void reducer()
-{
 
+
+void *reducer(void* ptr)
+{
+	
+	for (int i = 0; i < N; i++) 
+	{
+		//printf("hello\n");
+		while(sharedBufferStatusForAdder[i] != 1);
+		//printf("Finally...\n");
+
+	}
+
+	//printf("Hi\n");
+	queue* sharedBufferFinal = create_list();
+
+	for (int i = 0; i < N; i++) 
+	{
+		while(sharedBuffer[i]->size > 0) 
+		{
+			node* wordsList = dequeue(sharedBuffer[i]);
+
+			node* ret = look(sharedBufferFinal, wordsList->word);
+
+		 	if (ret == NULL) 
+		 	{
+		 		enqueue(sharedBufferFinal, wordsList->count, wordsList->word);
+		 	}
+		 	else 
+		 	{
+		 		ret->count += wordsList->count;
+		 	}
+		 	free(wordsList);
+		}
+
+	}
+	FILE* file = fopen("output.txt", "rw+");
+
+	//printf("1\n");
+	node *current_node = sharedBufferFinal->head;
+
+	while(current_node)
+	{
+		//printf("Hello \n");
+		fprintf(file, "%s, %d\n", current_node->word, current_node->count);
+		current_node = current_node->next;
+	}
+
+
+	close_file(file);
+
+
+	pthread_exit(NULL);
 }
 
 int main(int args, char* argv[])
 {
-	int c, i;
+	//int c;
 	extern char *optarg;
 	extern int optind, optopt;
 
 	int file_;
+	int flag =0;
 	double size;
 
-	word_list word;
+	N=0, B=0;
 
-	N=0; B=0;
+	N = atoi(argv[2]);
+	B = atoi(argv[3]);
 
-	while ((c = getopt(args, argv, "n:b:")) != -1) 
+	if(args < 3)
 	{
-		switch(c)
-		{
-			case 'n': 
-					N = atoi(optarg); 
-					break;
-			case 'b': 
-					B = atoi(optarg); 
-					break;
-		}
+		perror("Please enter the (filname) (threads) (buffer size)\n");
 	}
 
 	//char filename[50];
 
 	//strcpy(filename, argv[optind]);
 
-	FILE *file = fopen(argv[optind], "r");
+
+	//timer to test
+	timer_start = timer();
+
+
+	FILE *file = fopen(argv[1], "r");
 
 	if(file == NULL)
 	{
@@ -96,9 +282,14 @@ int main(int args, char* argv[])
 
 	printf("Size of the file is: %f\n", size);
 
-	char *buffer = mmap( 0, size , PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, file_, 0 );
+	char *buffer = mmap( 0, size+1 , PROT_READ | PROT_WRITE, MAP_PRIVATE , file_, 0 );
 
-	i=0;
+	sharedBuffer = malloc(sizeof(queue *) * N);
+	sharedBufferStatusForAdder = malloc(sizeof(int*)*N);
+
+
+	init();
+	/*	i=0;
 	char *chunk;
 
 	int min_chunk_size = size / N;
@@ -129,23 +320,100 @@ int main(int args, char* argv[])
 		printf( "where_we_are: %d, chunkSize: %d\n", where_we_are, chunkSize);
 				
 	}
+*/
 
+	//printf("%s\n", buffer);
+
+	int where_it_went = 0;
+	int chunkSize = size / N;
+	int where_we_are = 0; 
+
+	threads = malloc(sizeof(pthread_t)* N);
+
+	for(int z = 0; z < N && where_we_are < size; z++)	//working until the number of N
+	{
+		char *chunk;
+		int curChunkSize = chunkSize;
+		int start = where_it_went;
+		int current = 0;
+
+		if( (start + chunkSize) > size)
+		{
+			current = size;			
+		}
+		else
+		{
+			current = start + chunkSize;
+		}
+
+		while (current != size && buffer[current] != ' ' && buffer[current] != '\0' && buffer[current] != '\n') 
+		{
+			current++;
+			curChunkSize++;
+		}
+		where_it_went = current;
+
+
+		printf("%d current : %d where it went: %d\n",curChunkSize, current, where_it_went );
+
+
+		chunk = malloc(sizeof(char)*curChunkSize + 10);
+
+		int end_point = start + curChunkSize;
+		for (int i = start; i < end_point; i++)
+		{
+			chunk[i-start] = buffer[i];
+			if(i == curChunkSize-1)
+			{
+				chunk[curChunkSize] = '\0';
+			}
+		}
+
+		//printf("%s\n", chunk);
+		wordBuffer wordReader;
+		wordReader.string = malloc(sizeof(char)*curChunkSize);
+		wordReader.string = chunk;
+		wordReader.index = z;
+
+		//threads[z] = malloc(sizeof(pthread_t));
+
+		pthread_create(&threads[z], NULL, map_reader_i, &wordReader);
+		//pthread_create(&threads[z+1], NULL, map_adder_i, &wordReader);
+
+	for (int i = 0; i < N; ++i)
+	{
+		pthread_join(threads[i], NULL);
+	}
+		flag++;
+	}
+
+
+	free(threads);
+
+	close_file(file);
+
+	while(flag < N)
+	{
+		sharedBufferStatusForAdder[flag] = 1;
+		flag++;
+	}
+
+	printf("Threads have done processing\n");
+
+	
+	pthread_t reducer_thread;
+	pthread_create(&reducer_thread, NULL, reducer, NULL);
+
+	timer_end = timer();
+
+
+	
+	printf("Time taken : %f\n", timer_end - timer_start);
+	
+	pthread_exit(NULL);
+
+
+	
 	return 0;
 }
 
-i = 0;
-	int MIN_CHUNK_SIZE = length / THREADS;
-	int index = 0; // thread number
-	while (i < length && index < THREADS) {
-		char *chunk;
-		int curChunkSize = MIN_CHUNK_SIZE;
-		int start = i;
-		int end = min(start + MIN_CHUNK_SIZE, length);
-
-		// get chunksize by iterating to end of word from MIN_CHUNK_SIZE
-		while (end != length && buf[end] != ' ' && buf[end] != '\0' && buf[end] != '\n') {
-			end++;
-			curChunkSize++;
-		}
-		printf("%d\n",curChunkSize );
-		i = end;
--- cmpsc473mr.h
+++ cmpsc473mr.h
@@ -15,17 +15,128 @@
 
 
 typedef struct __word_list{
-	char word[50];
+	char *word;
 	int count;
+	struct __word_list *next;
+	struct __word_list *tail;
+	struct __word_list *head;
+	int size;
 }word_list;
 
 typedef struct __list{
-	char string[30];
+	char *string;
+	int index;
 }wordBuffer;
 
+typedef struct __this_is_same_as_word_list
+{
+	char* word;
+	int count;
+	struct __this_is_same_as_word_list *next;
+}node;
+
+typedef struct queue
+{
+    node *head;
+    node *tail;
+    int size;
+}queue;
+
+typedef struct __adderStruct{
+	pthread_mutex_t lock;
+	int count;
+	word_list *list;
+	queue *adderlist;
+	int read_before;
+	int index;
+}adderBuffer;
+
+
+
+typedef struct rdata_t {
+	char* chunk;
+	int index;
+} readerBuffer; 
+
+
+static double timer() {    
+    struct timespec tp;
+    clock_gettime(CLOCK_MONOTONIC, &tp);
+    return ((double) (tp.tv_sec) + 1e-9 * tp.tv_nsec);
+}
 
 void *map_reader_i(void*);
 
 void *map_adder_i(void*);
 
-void reducer();
+void *reducer(void*);
+
+void init();
+
+queue *create_list ( )
+{
+	queue *list = malloc( sizeof( queue ) );
+	list->size = 0;
+	list->head = list->tail = NULL;
+	return list;
+}
+
+
+void enqueue( queue *list, int count, char *word )
+{
+	node *current = (node*)malloc(sizeof(node));
+	current->word = malloc( sizeof( char* ) *(strlen( word ) + 1 ) ); // strlen does not include null terminator
+	current->count = count;
+
+	strcpy( current->word , word );
+	current->next = NULL;
+
+	if (list->tail == NULL )
+	{ //empty
+		list->head = current;
+		list->tail = current;
+	} 
+	else {
+		list->tail->next = current; //point tail to new node
+		list->tail = current;
+	}
+	list->size++;
+}
+
+node *dequeue (  queue *list )
+{
+	 node *current = list->head;
+	 node *prev = NULL;
+	if (current == NULL ){ // empty
+		return NULL;
+	}
+	while( current->next != NULL ) 
+	{ // iterate to end
+		prev = current;
+		current = current->next;
+	}
+	if (current != list->head ) {
+		prev->next = NULL;	
+		list->tail = prev;
+	}
+	else {
+		list->head = list->tail = NULL; // one element
+	}
+	list->size--;
+	return current;
+}
+
+
+node *look (queue *list, char *word)
+{
+	node *current = list->head;
+	if (current == NULL )
+		return NULL;
+	while (current->next )
+	{
+		if (strcmp(word, current->word) == 0) 
+			return current;
+		current = current->next;
+	}
+	return NULL;
+}
